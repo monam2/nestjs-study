d7695cf604c9adb2e560537b543d2a746b67f856
monam2 <kangcw0107@gmail.com>
2025-03-31 20:08:23 +0900

    init

---
+++ netflix/src/app.controller.ts
@@ -0,0 +1,68 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+
+interface Movie {
+  id: number;
+  title: string;
+}
+
+@Controller('movie')
+export class AppController {
+  private movies: Movie[] = [
+    {
+      id: 1,
+      title: '해리포터',
+    },
+    {
+      id: 2,
+      title: '반지의 제왕',
+    },
+  ];
+
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getMovies() {
+    return this.movies;
+  }
+
+  @Get('/:id')
+  getMovie(@Param('id') id: string) {
+    const movie = this.movies.find((m) => m.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 영화입니다.');
+    }
+
+    return movie;
+  }
+
+  @Post()
+  postMovie() {
+    return {
+      id: 3,
+      title: '어벤져스',
+    };
+  }
+
+  @Patch('/:id')
+  patchMovie() {
+    return {
+      id: 3,
+      title: '어벤져스',
+    };
+  }
+
+  @Delete('/:id')
+  deleteMovie() {
+    return 3;
+  }
+}
