23524fd58711b141307ad1da6551754cb746dfae
monam2 <kangcw0107@gmail.com>
2025-04-08 22:01:41 +0900

    movie 엔티티

--- netflix/src/movie/entity/movie.entity.ts
+++ netflix/src/movie/entity/movie.entity.ts
@@ -1,10 +1,29 @@
-import { Exclude, Expose } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  VersionColumn,
+} from 'typeorm';
 
+@Entity()
 export class Movie {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   title: string;
 
-  @Expose()
-  @Exclude()
+  @Column()
   genre: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @VersionColumn()
+  version: number;
 }
