9c433ce5f6dd183e7cd5cfa7b108c8dcc0b00d4e
monam2 <kangcw0107@gmail.com>
2025-04-10 21:41:27 +0900

    detail 엔티티 관계 추가

--- netflix/src/movie/entity/movie.entity.ts
+++ netflix/src/movie/entity/movie.entity.ts
@@ -1,25 +1,19 @@
 import {
   Column,
-  CreateDateColumn,
   Entity,
+  JoinColumn,
+  OneToOne,
   PrimaryGeneratedColumn,
-  UpdateDateColumn,
-  VersionColumn,
 } from 'typeorm';
+import { BaseTable } from './base.entity';
+import { MovieDetail } from './movie-detail.entity';
 
-export class BaseEntity {
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-
-  @VersionColumn()
-  version: number;
-}
+// ManyToOne - Director: 감독은 여러 개의 영화를 만들 수 있다.
+// OneToOne - MovieDetail: 영화는 하나의 상세 내용을 갖을 수 있다.
+// ManyToMany - Genre: 영화는 여러개의 장르를 가질 수 있고, 장르는 여러 개의 영화에 속할 수 있음.
 
 @Entity()
-export class Movie extends BaseEntity {
+export class Movie extends BaseTable {
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -28,4 +22,8 @@ export class Movie extends BaseEntity {
 
   @Column()
   genre: string;
+
+  @OneToOne(() => MovieDetail)
+  @JoinColumn()
+  detail: MovieDetail;
 }
